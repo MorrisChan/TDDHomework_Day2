5af918b6b1ff5df1a8fc8ed30a632b8a5a16faf0
Morris Chan <morris.ccs@gmail.com>
2016-07-12 23:23:19 +0800

    First failed Test Case

---
+++ TDDHomework_Day2.Tests/PotterShoppingCartTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDDHomework_Day2.Tests
+{
+    [TestClass]
+    public class PotterShoppingCartTests
+    {
+        [TestMethod]
+        public void Buy_a_Harry_Potter_Episode_1_TotalAmount_should_be_100()
+        {
+            //arrange
+            var target = new PotterShoppingCart();
+            target.Add("episode1", 1);
+            var expected = 100;
+            //actual
+            var actual = target.CalculateTotalAmount();
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ TDDHomework_Day2/PotterShoppingCart.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDDHomework_Day2
+{
+    public class PotterShoppingCart
+    {
+        private Dictionary<string, int> Cart = new Dictionary<string, int>();
+        private int Price = 100;
+
+
+        public void Add(string episode, int buyCount)
+        {
+            if(Cart.Any(a => a.Key == episode))
+            {
+                Cart[episode] += buyCount;
+            }
+            else
+            {
+                Cart.Add(episode, buyCount);
+            }
+        }
+
+        public object CalculateTotalAmount()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    
+}
