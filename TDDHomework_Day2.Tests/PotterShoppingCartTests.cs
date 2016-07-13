905c7b572901fcada7a74443be95ad994640a12a
ci9 <ci9@winton.com.tw>
2016-07-13 14:49:10 +0800

    Failed Buy HarryPotter ep1 ep2 ep3 TotalAmount should be 270

--- TDDHomework_Day2.Tests/PotterShoppingCartTests.cs
+++ TDDHomework_Day2.Tests/PotterShoppingCartTests.cs
@@ -32,5 +32,19 @@ namespace TDDHomework_Day2.Tests
             //assert
             Assert.AreEqual(expected, actual);
         }
+        [TestMethod]
+        public void Buy_a_Harry_Potter_Episode_1_2_3_TotalAmount_should_be_270()
+        {
+            //arrange
+            var target = new PotterShoppingCart();
+            target.Add("episode1", 1);
+            target.Add("episode2", 1);
+            target.Add("episode3", 1);
+            double expected = 270;
+            //actual
+            var actual = target.CalculateTotalAmount();
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
